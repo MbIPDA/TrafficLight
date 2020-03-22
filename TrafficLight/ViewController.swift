4671d0db2ec7f62a6c2bb65232988ce238b63e07
Sergei Shumin <mbipda@yandex.ru>
2020-03-22 13:32:58 +0000

    1. traffic light added

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLight: UIStackView!
+    @IBOutlet var redSignal: UIView!
+    @IBOutlet var yellowSignal: UIView!
+    @IBOutlet var greenSignal: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLight.isHidden = false
+        redSignal.alpha = 0.3
+        yellowSignal.alpha = 0.3
+        greenSignal.alpha = 0.3
+        redSignal.layer.cornerRadius = 64
+        yellowSignal.layer.cornerRadius = 64
+        greenSignal.layer.cornerRadius = 64
     }
 
 
